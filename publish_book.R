2e45a8b38be29668b3f44920c74ab84d22631473
Marc Teunis <marc.teunis@hu.nl>
2020-12-19 19:52:23 +0100

    add manifest

---
+++ publish_book.R
@@ -0,0 +1,10 @@
+dir_out <- here::here("simple_bookdown")
+
+bookdown::render_book(
+  input = here::here(),
+  output_dir = dir_out)
+## create manifest for github continous integration updates the bookdown website
+## upon push to master
+
+rsconnect::writeManifest(
+  appPrimaryDoc = "index.Rmd")
